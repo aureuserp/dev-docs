496ce486d5e3fa78e815abc4e4c55cca7b483f1c
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-03-12 15:47:10 +0530

    update master route links to use absolute paths for consistency

--- .vitepress/routes/master.ts
+++ .vitepress/routes/master.ts
@@ -3,11 +3,11 @@ const masterRoute = [
     text: 'Prologue',
     collapsed: false,
     items: [
-      { text: 'Introduction', link: 'master/prologue/introduction' },
-      { text: 'Upgrade Guide', link: 'master/prologue/upgrade-guide' },
+      { text: 'Introduction', link: '/master/prologue/introduction' },
+      { text: 'Upgrade Guide', link: '/master/prologue/upgrade-guide' },
       {
         text: 'Contribution Guide',
-        link: 'master/prologue/contribution-guide'
+        link: '/master/prologue/contribution-guide'
       }
     ]
   },
@@ -15,40 +15,31 @@ const masterRoute = [
     text: 'Installation',
     collapsed: false,
     items: [
-      { text: 'Introduction', link: 'master/installation/introduction' },
-      { text: 'Requirements', link: 'master/installation/requirements' },
-      { text: 'Installation', link: 'master/installation/installation' },
-      { text: 'Docker', link: 'master/installation/docker' }
+      { text: 'Introduction', link: '/master/installation/introduction' },
+      { text: 'Requirements', link: '/master/installation/requirements' },
+      { text: 'Installation', link: '/master/installation/installation' },
+      { text: 'Docker', link: '/master/installation/docker' }
     ]
   },
   {
     text: 'Architecture Concepts',
     collapsed: false,
     items: [
-      { text: 'Introduction', link: 'master/architecture/introduction' },
+      { text: 'Introduction', link: '/master/architecture/introduction' },
       {
         text: 'Plugins',
         collapsed: true,
         items: [
           {
             text: 'Introduction',
-            link: 'master/architecture/plugins/introduction'
-          },
-          { text: 'Plugins', link: 'master/architecture/plugins/plugins' },
-          {
-            text: 'Frontend',
-            link: 'master/architecture/plugins/frontend'
-          },
-          {
-            text: 'Modular Design',
-            link: 'master/architecture/plugins/modular-design'
+            link: '/master/architecture/plugins/introduction'
           }
         ]
       },
-      { text: 'Frontend', link: 'master/architecture/frontend' },
+      { text: 'Frontend', link: '/master/architecture/frontend' },
       {
         text: 'Modular Design',
-        link: 'master/architecture/modular-design'
+        link: '/master/architecture/modular-design'
       }
     ]
   },
@@ -58,18 +49,18 @@ const masterRoute = [
     items: [
       {
         text: 'Getting started',
-        link: 'master/resources/getting-started'
+        link: '/master/resources/getting-started'
       },
       {
         text: 'Listing Records',
-        link: 'master/resources/listing-records'
+        link: '/master/resources/listing-records'
       },
       {
         text: 'Creating Record',
-        link: 'master/resources/creating-record'
+        link: '/master/resources/creating-record'
       },
-      { text: 'Editing Record', link: 'master/resources/editing-record' },
-      { text: 'Viewing Record', link: 'master/resources/viewing-record' }
+      { text: 'Editing Record', link: '/master/resources/editing-record' },
+      { text: 'Viewing Record', link: '/master/resources/viewing-record' }
     ]
   }
 ]
